f6faec424f4f0dd63ab593216a850664af2183b1
Lewis Nozaki <lhnozaki@outlook.com>
2021-10-24 20:23:58 -1000

    updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# MERN-bank
+
+A simple MERN stack application simulating an online bank.
---
+++ server/server.js
@@ -0,0 +1,32 @@
+// Imports
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const rootDir = require("./helpers/path.helper");
+const cookieParser = require("cookie-parser");
+
+// Routes imports
+const homeRouter = require("./routes/home.routes");
+
+
+require("dotenv").config({ path: path.join(rootDir, "secure", ".env") });
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Database connection & app start
+mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then(result => {
+    console.log("connected to MongoDB Atlas");
+    app.listen(PORT, () => console.log("server running on port", PORT));
+  })
+  .catch(err => console.log(err));
+
+// Routes
+app.use(homeRouter);
+
